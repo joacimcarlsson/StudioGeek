d3f25c2b14800778357b2ea4c050b0f15811e56b
Joacim Carlsson <joacimcarlsson@me.com>
2012-01-17 15:06:56 +0100

    mera test

---
+++ Nya studio appen/Nya studio appen/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Nya studio appen
+//
+//  Created by Joacim Carlsson on 2012-01-12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
